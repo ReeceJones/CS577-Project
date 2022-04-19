b57c5c4d12c99b5e1c27f68c3df61b58f46486e1
ReeceJones <reece_jones@icloud.com>
2022-04-19 11:07:40 -0400

    Add transcript matching

--- scrape_games.py
+++ scrape_games.py
@@ -1,8 +1,10 @@
+from datetime import date
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 import re
 import json
 import argparse
+from numpy import int64
 import requests
 import time
 import pandas as pd
@@ -28,8 +30,9 @@ def dump_games(json_data):
     for k,v in json_data.items():
         teams = list(map(lambda x: re.sub('[_]', ' ', x), v['teams']))
         year = v['year']
-        matches.append((teams[0], teams[1], year))
-    return pd.DataFrame(matches, columns=['home_team', 'visiting_team', 'year'])
+        transcript = v['transcript']
+        matches.append((teams[0], teams[1], year, transcript))
+    return pd.DataFrame(matches, columns=['home_team', 'visiting_team', 'year', 'transcript'])
 
 def unbreak_html_text(html_text):
     # lol
@@ -51,20 +54,6 @@ def parse_table(table):
         table_data.append(row_data)
     return table_data
 
-def get_game(team, date):
-    formatted_string = time.strftime(f'https://www.pro-football-reference.com/boxscores/%Y%m%d0{team}.htm', date)
-    # req = pool_manager.request('GET', formatted_string)
-    req = requests.get(formatted_string)
-    if req.status_code == 200:
-        html = unbreak_html_text(req.text)
-        # print(html)
-        parsed_html = BeautifulSoup(html, 'html.parser')
-        table = parsed_html.find('table', id='pbp')
-        table_data = parse_table(table.find('tbody'))
-        return pd.DataFrame(table_data, columns=['quarter', 'time', 'down', 'togo', 'location', 'detail', 'away_points', 'home_points', 'epb', 'epa'])
-    print('Error occureced scraping:', req.status, formatted_string)
-    return None
-
 def get_manifest(query_string):
     req = requests.get(query_string)
     if req.status_code == 200:
@@ -73,8 +62,8 @@ def get_manifest(query_string):
         table = parsed_html.find(id='games').find('tbody')
         table_data = parse_table(table)
         print(f'Scraped {query_string}')
-        return pd.DataFrame(table_data, columns=['rk', 'week', 'day', 'date', 'unk0', 'winner', 'unk1', 'loser', 'unk2', 'points_winner', 'points_loser', 'yards_winner', 'tow', 'yards_loswer', 'tol'])
-    print('Error occured scraping:', req.status, query_string)
+        return pd.DataFrame(table_data, columns=['rk', 'week', 'day', 'date', 'outcome', 'winner', 'visit_indicator', 'loser', 'score_method', 'points_winner', 'points_loser', 'yards_winner', 'tow', 'yards_loswer', 'tol'])
+    print('Error occured scraping:', req.status_code, query_string)
     return None
 
 def get_manifests():
@@ -89,12 +78,76 @@ def get_manifests():
         tables = list(p.map(get_manifest, queries))
         return pd.concat(tables)
     return None
-        
+
+def join_matches(matches, manifest, search_list):
+    # preprocess team names
+    a = manifest.copy()
+    a.winner = a.winner.str.lower()
+    a.loser = a.loser.str.lower()
+    b = search_list.copy()[['short', 'long']]
+    b.long = b.long.str.lower()
+
+    # make team names use home/away schema
+    def order_teams(x):
+        home = x.loser if x.visit_indicator == '@' else x.winner
+        away = x.winner if x.visit_indicator == '@' else x.loser
+        date = pd.to_datetime(x.date, format='%Y-%m-%d')
+        return pd.Series({'date': date, 'year': date.year, 'home_team': home, 'visiting_team': away})
+    c = a.apply(order_teams, axis=1)
+
+    matches.year = matches.year.apply(lambda x: int(x))
+
+    # now join FOOTBALL matches with pfr matches to get exact date
+    d = pd.merge(matches, c, how='inner', on=['home_team', 'visiting_team', 'year'])
+
+    # now join with pfr team search list to get search code
+    e = dict()
+    for idx, x in b.iterrows():
+        e[x.long] = x.short
+
+    d['home_short'] = d.home_team.replace(e)
+    d['visiting_short'] = d.visiting_team.replace(e)
+
+    return d
+
+def get_game(t):
+    # formatted_string = time.strftime(f'https://www.pro-football-reference.com/boxscores/%Y%m%d0{team}.htm', date)
+    # req = pool_manager.request('GET', formatted_string)
+    query_string, row = t
+    req = requests.get(query_string)
+    if req.status_code == 200:
+        html = unbreak_html_text(req.text)
+        # print(html)
+        parsed_html = BeautifulSoup(html, 'html.parser')
+        table = parsed_html.find('table', id='pbp')
+        if table is not None:
+            table_data = parse_table(table.find('tbody'))
+            print(f'Scraped {query_string}')
+            j = dict()
+            j['pbp'] = json.loads(pd.DataFrame(table_data, columns=['quarter', 'time', 'down', 'togo', 'location', 'detail', 'away_points', 'home_points', 'epb', 'epa']).to_json(orient='records'))
+            j['transcript'] = row.transcript
+            j['date'] = row.date.timetuple()
+            j['teams'] = [row.home_team, row.visiting_team]
+            return j
+    print('Error occureced scraping:', req.status_code, row.home_team, row.visiting_team, query_string)
+    return None
+
+def scrape_matches(joined_matches):
+    queries = list()
+    for idx, row in joined_matches.iterrows():
+        query_string = time.strftime(f'https://www.pro-football-reference.com/boxscores/%Y%m%d0{row.home_short}.htm', row.date.timetuple())
+        queries.append((query_string, row))
+    with Pool(SCRAPE_PROCESSES) as p:
+        matches = list(p.map(get_game, queries))
+        return matches
+    return None
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dump-games', action='store_true', dest='dump_games')
     parser.add_argument('--scrape-manifest', action='store_true', dest='scrape_manifest')
+    parser.add_argument('--scrape-games', action='store_true', dest='scrape_games')
     parser.add_argument('--scrape-processes', action='store', dest='scrape_processes', default=SCRAPE_PROCESSES)
     args = parser.parse_args()
     SCRAPE_PROCESSES = args.scrape_processes
@@ -104,9 +157,21 @@ if __name__=='__main__':
         if args.dump_games:
             matches.to_csv('dataset/matches.csv', index=False)
 
+        manifest = None
         if args.scrape_manifest:
             manifest = get_manifests()
             manifest.to_csv('dataset/game_manifest.csv', index=False)
+        else:
+            manifest = pd.read_csv('dataset/game_manifest.csv')
+
+        if args.scrape_games:
+            search_list = pd.read_csv('dataset/teams_search_list.csv', names=['short', 'long', 'period', 'unk0', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5'])
+            joined_matches = join_matches(matches, manifest, search_list)
+            # print(len(joined_matches))
+            scraped_matches = [x for x in scrape_matches(joined_matches) if x is not None]
+            print(f'{len(scraped_matches)} transcripts matched')
+            with open('dataset/matched_transcripts.json', 'w') as f:
+                f.write(json.dumps(scraped_matches))
 
         # game_pbp = get_game('gnb', time.strptime('20141109', '%Y%m%d'))
         # game_pbp.to_csv('test_gnb.csv', index=False)
